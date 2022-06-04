107a88ebdeca809aa011842aa07cf7fcdccf5bc5
Matthias Karlbauer <karlbam@bronze.atmos.washington.edu>
2022-06-04 10:23:29 -0700

    Added capped exponential linear unit (ELU).

--- dlwp/model/layers/activations.py
+++ dlwp/model/layers/activations.py
@@ -18,3 +18,22 @@ class CappedLeakyReLU(torch.nn.Module):
         x = self.relu(inputs)
         x = torch.clamp(x, max=self.cap)
         return x
+
+
+class CappedELU(torch.nn.Module):
+    """
+    Implements an ELU with capped maximum value.
+    """
+    def __init__(self, cap_value=1., **kwargs):
+        """
+        :param cap_value: float: value at which to clip activation
+        :param kwargs: passed to torch.nn.ELU
+        """
+        super().__init__()
+        self.add_module('elu', torch.nn.ELU(**kwargs))
+        self.cap = torch.tensor(cap_value, dtype=torch.float32)
+
+    def forward(self, inputs):
+        x = self.elu(inputs)
+        x = torch.clamp(x, max=self.cap)
+        return x
