975fbbae56097917c6c22211f7c85a9f0a29a75e
Matthias Karlbauer <karlbam@uw.edu>
2022-09-04 15:23:28 -0700

    Added HEALPix unet3plus

--- dlwp/model/models/__init__.py
+++ dlwp/model/models/__init__.py
@@ -1,4 +1,5 @@
 from .unet import CubeSphereUnet
 from .dgmr import CubeSphereUnetDGMR
+from .hpx_unet import HEALPixUnet
 
-__all__ = ['CubeSphereUnet', 'CubeSphereUnetDGMR']
+__all__ = ['CubeSphereUnet', 'CubeSphereUnetDGMR', 'HEALPixUnet']
