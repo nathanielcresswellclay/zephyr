b32bf55c2ccbfa58339ada9c4249c2fa65a21005
nathanielcresswellclay <n.cresswell09@gmail.com>
2023-01-22 19:23:12 -0800

    removed references to HEALPixUnetSSIM class (this is now replaced by custom loss class

--- training/dlwp/model/models/__init__.py
+++ training/dlwp/model/models/__init__.py
@@ -1,4 +1,4 @@
 from .unet import CubeSphereUnet
-from .hpx_unet import HEALPixUnet,HEALPixUnetSSIM,HEALPixUnetCustomLoss
+from .hpx_unet import HEALPixUnet,HEALPixUnetCustomLoss
 
-__all__ = ['CubeSphereUnet', 'HEALPixUnet', 'HEALPixUnetSSIM', 'HEALPixUnetCustomLoss']
+__all__ = ['CubeSphereUnet', 'HEALPixUnet', 'HEALPixUnetCustomLoss']
