40a623e27c4a8444aa445c565a84c6e07097f7b2
nathanielcresswellclay <n.cresswell09@gmail.com>
2023-06-12 09:09:07 -0700

    added static file to analyses routines that stores conveientient conventions

---
+++ analyses/statics.py
@@ -0,0 +1,12 @@
+# This module contains static values that can be referenced throughout the package 
+
+variable_labels = {
+    'z500' : 'Z$_{500}$',
+    'z1000' : 'Z$_{1000}$',
+    't2m' : 'T$_{2}$',
+    't850' : 'T$_{850}$',
+}
+
+
+
+
