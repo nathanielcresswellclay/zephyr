3b6d4ec869e79c0386ef97e4d9895c8e44365031
nathanielcresswellclay <n.cresswell09@gmail.com>
2023-09-29 14:31:49 -0700

    added unet3+ architecture to available models

--- training/dlwp/model/models/__init__.py
+++ training/dlwp/model/models/__init__.py
@@ -1,3 +1,4 @@
 from .unet import CubeSphereUNet, HEALPixUNet, HEALPixRecUNet
+from .unet3plus import HEALPixUNet3Plus
 
-__all__ = ['CubeSphereUNet', 'HEALPixUNet', 'HEALPixRecUNet']
+__all__ = ['CubeSphereUNet', 'HEALPixUNet', 'HEALPixRecUNet', 'HEALPixUNet3Plus']
