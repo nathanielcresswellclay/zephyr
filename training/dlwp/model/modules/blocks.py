0d1f77015f693f70fa5e61f7840176b99b1ce3d2
nathanielcresswellclay <n.cresswell09@gmail.com>
2023-11-03 11:26:14 -0700

    added a double convnext block as discussed with Dale

--- training/dlwp/model/modules/blocks.py
+++ training/dlwp/model/modules/blocks.py
@@ -216,6 +216,131 @@ class MobileNetConvBlock(th.nn.Module):
     def forward(self, x):
         return self.skip_module(x) + self.convblock(x)
 
+class DoubleConvNeXtBlock(th.nn.Module):
+    """
+    Another modification of ConvNeXtBlock block this time putting two into a single block
+    """
+    def __init__(
+            self,
+            geometry_layer: th.nn.Module = HEALPixLayer,
+            in_channels: int = 3,
+            latent_channels: int = 1,
+            out_channels: int = 1,
+            kernel_size: int = 3,
+            dilation: int = 1,
+            upscale_factor: int = 4,
+            n_layers: int = 1,
+            activation: th.nn.Module = None,
+            enable_nhwc: bool = False,
+            enable_healpixpad: bool = False
+            ):
+        super().__init__()
+    
+        if in_channels == int(latent_channels):
+            self.skip_module1 = lambda x: x  # Identity-function required in forward pass
+        else:
+            self.skip_module1 = geometry_layer(
+                layer='torch.nn.Conv2d',
+                in_channels=in_channels,
+                out_channels=int(latent_channels),
+                kernel_size=1,
+                enable_nhwc=enable_nhwc,
+                enable_healpixpad=enable_healpixpad
+                )
+        if out_channels == int(latent_channels):
+            self.skip_module2 = lambda x: x  # Identity-function required in forward pass
+        else:
+            self.skip_module2 = geometry_layer(
+                layer='torch.nn.Conv2d',
+                in_channels=int(latent_channels),
+                out_channels=out_channels,
+                kernel_size=1,
+                enable_nhwc=enable_nhwc,
+                enable_healpixpad=enable_healpixpad
+                )
+
+        # 1st ConvNeXt block, the output of this one remains internal 
+        convblock1 = []
+        # 3x3 convolution establishing latent channels channels
+        convblock1.append(geometry_layer(
+            layer='torch.nn.Conv2d',
+            in_channels=in_channels,
+            out_channels=int(latent_channels),
+            kernel_size=kernel_size,
+            dilation=dilation,
+            enable_nhwc=enable_nhwc,
+            enable_healpixpad=enable_healpixpad
+            ))
+        if activation is not None: convblock1.append(activation)
+        # 1x1 convolution establishing increased channels
+        convblock1.append(geometry_layer(
+            layer='torch.nn.Conv2d',
+            in_channels=int(latent_channels),
+            out_channels=int(latent_channels*upscale_factor),
+            kernel_size=1,
+            dilation=dilation,
+            enable_nhwc=enable_nhwc,
+            enable_healpixpad=enable_healpixpad
+            ))
+        if activation is not None: convblock1.append(activation)
+        # 1x1 convolution returning to latent channels
+        convblock1.append(geometry_layer(
+            layer='torch.nn.Conv2d',
+            in_channels=int(latent_channels*upscale_factor),
+            out_channels=int(latent_channels),
+            kernel_size=1,
+            dilation=dilation,
+            enable_nhwc=enable_nhwc,
+            enable_healpixpad=enable_healpixpad
+            ))
+        if activation is not None: convblock1.append(activation)
+        self.convblock1 = th.nn.Sequential(*convblock1)
+
+        # 2nd ConNeXt block, takes the output of the first convnext block 
+        convblock2 = []
+        # 3x3 convolution establishing latent channels channels
+        convblock2.append(geometry_layer(
+            layer='torch.nn.Conv2d',
+            in_channels=int(latent_channels),
+            out_channels=int(latent_channels),
+            kernel_size=kernel_size,
+            dilation=dilation,
+            enable_nhwc=enable_nhwc,
+            enable_healpixpad=enable_healpixpad
+            ))
+        if activation is not None: convblock2.append(activation)
+        # 1x1 convolution establishing increased channels
+        convblock2.append(geometry_layer(
+            layer='torch.nn.Conv2d',
+            in_channels=int(latent_channels),
+            out_channels=int(latent_channels*upscale_factor),
+            kernel_size=1,
+            dilation=dilation,
+            enable_nhwc=enable_nhwc,
+            enable_healpixpad=enable_healpixpad
+            ))
+        if activation is not None: convblock2.append(activation)
+        # 1x1 convolution reducing to output channels
+        convblock2.append(geometry_layer(
+            layer='torch.nn.Conv2d',
+            in_channels=int(latent_channels*upscale_factor),
+            out_channels=output_channels,
+            kernel_size=1,
+            dilation=dilation,
+            enable_nhwc=enable_nhwc,
+            enable_healpixpad=enable_healpixpad
+            ))
+        if activation is not None: convblock2.append(activation)
+    
+
+
+    def forward(self, x):
+        # internal convnext result 
+        x1 = self.skip_module1(x) + self.convblock1(x)
+        # return second convnext result 
+        return self.skip_module2(x1) + self.convblock2(x1)
+        
+
 
 class ConvNeXtBlock(th.nn.Module):
     """
@@ -262,7 +387,7 @@ class ConvNeXtBlock(th.nn.Module):
             enable_healpixpad=enable_healpixpad
             ))
         if activation is not None: convblock.append(activation)
-        # 3x3 convolution decreasing channels
+        # 3x3 convolution maintaining increased channels
         convblock.append(geometry_layer(
             layer='torch.nn.Conv2d',
             in_channels=int(latent_channels*upscale_factor),
