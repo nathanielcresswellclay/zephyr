0065301a30b597cb6a3fc8fbb86268ceffd47d97
nathanielcresswellclay <n.cresswell09@gmail.com>
2023-12-02 12:17:50 -0800

    updated training script

--- scripts/run_training.sh
+++ scripts/run_training.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
 
-RUN_CMD="python -u scripts/train.py num_workers=8 port=29452 learning_rate=2e-4 batch_size=16 experiment_name=test model=hpx_rec_unet model/modules/blocks@model.encoder.conv_block=conv_next_block model/modules/blocks@model.decoder.conv_block=conv_next_block model.encoder.n_channels=[64,32,16] model.decoder.n_channels=[16,32,64] trainer.max_epochs=300 data=era5_hpx32_7var_6h_24h data.src_directory=/home/mercury2/karlbam/Data/DLWP/HPX32 data.dst_directory=/home/mercury3/karlbam data.prefix=era5_1deg_3h_HPX32_1979-2021_  data.prebuilt_dataset=True data.module.drop_last=True trainer/lr_scheduler=cosine trainer/optimizer=adam model.enable_healpixpad=True"
-
-# Command to run model on CPU (useful for prototyping and verifying code)
-#NUM_CPU=4
-#srun -u --ntasks=1 \
-#     --ntasks-per-node=1 \
-#     --cpu_bind=sockets \
-#     -c $(( ${NUM_CPU} )) \
-#     bash -c "
-#     export WORLD_RANK=\${SLURM_PROCID}
-#     export HDF5_USE_FILE_LOCKING=True
-#     export CUDA_VISIBLE_DEVICES=
-#     export HYDRA_FULL_ERROR=1 
-#     ${RUN_CMD}"
-#exit
+RUN_CMD="python -u scripts/train.py num_workers=8 port=29450 learning_rate=2e-4 batch_size=8 experiment_name=hpx64_unet_136-68-34_cnxt_skip_dil_gru_6h_300 model=hpx_rec_unet model/modules/blocks@model.encoder.conv_block=conv_next_block model/modules/blocks@model.decoder.conv_block=conv_next_block model.encoder.n_channels=[136,68,34] model.decoder.n_channels=[34,68,136] trainer.max_epochs=300 data=era5_hpx64_7var_6h_24h data.dst_directory=/home/disk/quicksilver2/karlbam/Data/DLWP/HPX64 data.prefix=era5_0.25deg_3h_HPX64_1979-2021_ data.prebuilt_dataset=True data.module.drop_last=True trainer/lr_scheduler=cosine trainer/optimizer=adam model.enable_healpixpad=True"
 
 # Run configuration
 NUM_GPU=2
 NUM_CPU=16
 GPU_NAME=A100
-DEVICE_NUMBERS="0,1"
+DEVICE_NUMBERS="2,3"
 
 # Command to run model on 
 srun -u --ntasks=${NUM_GPU} \
